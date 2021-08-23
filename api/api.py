3ddeb47085e2ff5444efaa76839e26db7ff4020c
Arnoldo <juan.arnoldo.munoz@hotmail.com>
2021-08-23 10:02:26 -0500

    adding some comments

--- api/api.py
+++ api/api.py
@@ -4,7 +4,7 @@ from geopy.units import radians
 from .address import Address
 
 
-# function to search the given address
+# function to search the given address using the api
 def search_address(address: str) -> list:
     arr = []
     parameters = {
@@ -14,22 +14,25 @@ def search_address(address: str) -> list:
         "geocode": address,
         "results": 1
     }
-    # to check if the result exists
+    # to check if the result exists (this is to try to catch an empty object response)
     try:
         response = requests.get(
             "https://geocode-maps.yandex.ru/1.x/?", params=parameters)
         data = response.json()
         print("lo que quiero ver es: ", data.get("resoponse").get(
             "GeoObjectCollection").get("featureMember"))
+    # if the object was not empty, then:
     except:
         try:
-
+            # Here I used a for loop (in case we have more than 1 address response, but
+            # for now we only have 1 result).
+            # iterate over the json data response
             for i in (data.get('response').get('GeoObjectCollection').get(
                     'featureMember')):
-                # name of each result
+                # storing the name of each result
                 name = (i.get('GeoObject').get('metaDataProperty').get(
                     'GeocoderMetaData').get('text'))
-                # store coordinates
+                # storing coordinates
                 coord = (i.get('GeoObject').get('Point').get('pos'))
                 coord2 = coord.split(" ")
                 # store longitude and latitude
@@ -38,10 +41,11 @@ def search_address(address: str) -> list:
                 # create the address object to store the info and add it to array.
                 address_object = Address(name, lat, lon)
                 arr.append(address_object)
-
+            # return the list of address objects.
             return arr
-
+        # if there was an error return a message error.
         except:
             print(["There was an error. Send the request again."])
             return ["There was an error. Send the request again."]
+
     return arr
